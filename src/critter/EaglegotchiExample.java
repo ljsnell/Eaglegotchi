1e12c958eac321a7aecfa699d3683f69573134c7
ljsnell <ljsnell7@gmail.com>
2022-10-09 16:54:29 -0500

    Added icon and name

--- src/critter/EaglegotchiExample.java
+++ src/critter/EaglegotchiExample.java
@@ -1,6 +1,10 @@
 package critter;
 
+import java.awt.FlowLayout;
 import java.util.Scanner;
+import javax.swing.JFrame;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 
 public class EaglegotchiExample {
 
@@ -8,11 +12,22 @@ public class EaglegotchiExample {
 		try (Scanner scanner = new Scanner(System.in)) {
 			System.out.println("Enter your Eaglegotchi's Name:");
 			String eName = scanner.next();
-			
-			System.out.println("Select your Eaglegotchi's iconPath:");
-			String iconPath = scanner.next();
+			String iconPath = "egg.png";
 			Eaglegotchi gotchi = new Eaglegotchi(eName, "Happy", 3, iconPath);
 			System.out.println(gotchi.getIconPath());
+			JFrame frame = new JFrame();
+			frame.setLayout(new FlowLayout());
+			ImageIcon icon = new ImageIcon("egg.png");
+			JLabel name = new JLabel(eName);
+			JLabel colon = new JLabel(":");
+			JLabel label = new JLabel(icon);
+			frame.add(name);
+			frame.add(colon);
+			frame.add(label);
+			frame.setDefaultCloseOperation
+			       (JFrame.EXIT_ON_CLOSE);
+			frame.pack();
+			frame.setVisible(true);
 		}
 	}
 }
