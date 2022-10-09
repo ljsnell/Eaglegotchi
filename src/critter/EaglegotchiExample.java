5c9b3d216cce892a2215e16a7c0248fc8b8f7354
ljsnell <ljsnell@users.noreply.github.com>
2022-10-09 17:26:03 -0500

    Icons (#2)
    * Added icon and name
    
    * Update EaglegotchiExample.java
    
    Basic Home Screen

--- src/critter/EaglegotchiExample.java
+++ src/critter/EaglegotchiExample.java
@@ -1,6 +1,10 @@
 package critter;
 
+import java.awt.FlowLayout;
 import java.util.Scanner;
+import javax.swing.JFrame;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 
 public class EaglegotchiExample {
 
@@ -8,11 +12,22 @@ public class EaglegotchiExample {
 		try (Scanner scanner = new Scanner(System.in)) {
 			System.out.println("Enter your Eaglegotchi's Name:");
 			String eName = scanner.next();
-			
-			System.out.println("Select your Eaglegotchi's iconPath:");
-			String iconPath = scanner.next();
-			Eaglegotchi gotchi = new Eaglegotchi(eName, "Happy", 3, iconPath);
-			System.out.println(gotchi.getIconPath());
+			String iconPath = "egg.png";
+			Eaglegotchi gotchi = new Eaglegotchi(eName, "Indifferent", 3, iconPath);
+
+			JFrame frame = new JFrame();
+			frame.setLayout(new FlowLayout());
+			ImageIcon icon = new ImageIcon("egg.png");
+			JLabel nameLbl = new JLabel("Name: " +gotchi.getName());
+			JLabel moodLbl = new JLabel("Mood: " + gotchi.getMood());
+			JLabel label = new JLabel(icon);
+			frame.add(nameLbl);
+			frame.add(label);
+			frame.add(moodLbl);
+			frame.setDefaultCloseOperation
+			       (JFrame.EXIT_ON_CLOSE);
+			frame.pack();
+			frame.setVisible(true);
 		}
 	}
 }
