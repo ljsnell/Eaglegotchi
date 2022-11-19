2251bb90d7d8b1a531231a95b51164473fe03b54
ljsnell <ljsnell7@gmail.com>
2022-11-19 16:52:32 -0600

    New Layout
    And eagle icon.

--- src/critter/EaglegotchiExample.java
+++ src/critter/EaglegotchiExample.java
@@ -4,16 +4,18 @@ import java.awt.FlowLayout;
 import java.sql.SQLException;
 import java.util.Scanner;
 import javax.swing.JFrame;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import utils.DBHandler;
 
 public class EaglegotchiExample {
 
 	public static void main(String[] args) throws SQLException {
-		DBHandler dbHandler = new DBHandler();
-		Eaglegotchi usersGotchi = dbHandler.fetchEagleByUsername("LSNELL");		
+//		DBHandler dbHandler = new DBHandler();
+//		Eaglegotchi usersGotchi = dbHandler.fetchEagleByUsername("LSNELL");		
 		try (Scanner scanner = new Scanner(System.in)) {
 			System.out.println("Enter your Eaglegotchi's Name:");
 			String eName = scanner.next();
@@ -21,14 +23,18 @@ public class EaglegotchiExample {
 			Eaglegotchi gotchi = new Eaglegotchi(eName, "Indifferent", 3, iconPath);
 
 			JFrame frame = new JFrame();
+			JPanel jpanel = new JPanel();
+			jpanel.setLayout(new BoxLayout(jpanel, BoxLayout.Y_AXIS));
 			frame.setLayout(new FlowLayout());
-			ImageIcon icon = new ImageIcon("egg.png");
+			ImageIcon icon = new ImageIcon("eagle.png");
 			JLabel nameLbl = new JLabel("Name: " +gotchi.getName());
 			JLabel moodLbl = new JLabel("Mood: " + gotchi.getMood());
-			JLabel label = new JLabel(icon);
-			frame.add(nameLbl);
-			frame.add(label);
-			frame.add(moodLbl);
+			JLabel icon_label = new JLabel(icon);
+			frame.add(icon_label);
+			jpanel.add(nameLbl);
+			jpanel.add(moodLbl);
+			frame.add(jpanel);
+//			frame.add(moodLbl);
 			frame.setDefaultCloseOperation
 			       (JFrame.EXIT_ON_CLOSE);
 			frame.pack();
