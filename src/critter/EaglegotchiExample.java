2b356332bce0f7c49dd08cec2db30e9defd7ae64
ljsnell <ljsnell7@gmail.com>
2022-11-19 17:27:54 -0600

    Added placeholder buttons

--- src/critter/EaglegotchiExample.java
+++ src/critter/EaglegotchiExample.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 import javax.swing.JFrame;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -15,20 +16,30 @@ public class EaglegotchiExample {
 
 	public static void main(String[] args) throws SQLException {
 		DBHandler dbHandler = new DBHandler();
-		Eaglegotchi gotchi = dbHandler.fetchEagleByUsername("LSNELL");		
+		Eaglegotchi gotchi = dbHandler.fetchEagleByUsername("LSNELL");	
+
 		try (Scanner scanner = new Scanner(System.in)) {
 			JFrame frame = new JFrame();
 			JPanel jpanel = new JPanel();
 			jpanel.setLayout(new BoxLayout(jpanel, BoxLayout.Y_AXIS));
+
 			frame.setLayout(new FlowLayout());
-			ImageIcon icon = new ImageIcon("eagle.png");
+			ImageIcon icon = new ImageIcon(gotchi.getIconPath());
 			JLabel nameLbl = new JLabel("Name: " +gotchi.getName());
-			JLabel moodLbl = new JLabel("Mood: " + gotchi.getMood());
-			JLabel icon_label = new JLabel(icon);
-			frame.add(icon_label);
+			JLabel moodLbl = new JLabel("Mood: " + gotchi.getMood().trim() + "/10");
+			JLabel hungerLbl = new JLabel("Hunger: " + gotchi.getHungerLevel() + "/10");
+			JLabel iconLbl = new JLabel(icon);
+			JButton moodButton = new JButton("Play");
+			JButton hungerButton = new JButton("Feed");
+			
+			frame.add(iconLbl);
 			jpanel.add(nameLbl);
 			jpanel.add(moodLbl);
+			jpanel.add(hungerLbl);
+			jpanel.add(moodButton);
+			jpanel.add(hungerButton);
 			frame.add(jpanel);
+
 			frame.setDefaultCloseOperation
 			       (JFrame.EXIT_ON_CLOSE);
 			frame.pack();
